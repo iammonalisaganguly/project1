96d2ccb716a9b57aeca06d41da8a002923ae53ce
Raj141020 <rajnagwanshi1410@gmail.com>
2022-11-14 12:22:10 +0530

    added by Raj

--- src/index.js
+++ src/index.js
@@ -6,14 +6,15 @@ const app = express();
 
 app.use (bodyParser.json());
 
-let url = "" ;
+// let url = "" ;
 let port = process.env.PORT || 3000;
 
-mongoose.connect(url,{userNewUrlParser: true})
- .then(()=> console.log("MongoDb is connected"))
- .catch((err) => console.log(err));
+// mongoose.connect(url,{userNewUrlParser: true})
+//  .then(()=> console.log("MongoDb is connected"))
+//  .catch((err) => console.log(err));
 
  app.use("/",route);
+
  app.listen(port, function(){
     console.log("Express app running on port " + port);
  });
