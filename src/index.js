8b8a7de16811850befd3aa694062994c7951a82e
Raj141020 <rajnagwanshi1410@gmail.com>
2022-11-14 13:05:19 +0530

    added by Raj

--- src/index.js
+++ src/index.js
@@ -6,12 +6,12 @@ const app = express();
 
 app.use (bodyParser.json());
 
-let url = "mongodb+srv://project1-blog:ucDww7V0InVFB9oF@blog.y7qnwco.mongodb.net/?retryWrites=true&w=majority" ;
+// let url = "mongodb+srv://project1-blog:ucDww7V0InVFB9oF@blog.y7qnwco.mongodb.net/?retryWrites=true&w=majority" ;
 let port = process.env.PORT || 3000;
 
-mongoose.connect(url,{userNewUrlParser: true})
- .then(()=> console.log("MongoDb is connected"))
- .catch((err) => console.log(err));
+// mongoose.connect(url,{userNewUrlParser: true})
+//  .then(()=> console.log("MongoDb is connected"))
+//  .catch((err) => console.log(err));
 
  app.use("/",route);
 
